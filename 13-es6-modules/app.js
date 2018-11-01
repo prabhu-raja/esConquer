fb2f2f57bc6a551941d4396fa08aeb05fca1b4f5
prabhu-raja <prabhuraja@gmail.com>
2018-10-31 22:03:21 -0400

    import export 🎫

--- 13-es6-modules/app.js
+++ 13-es6-modules/app.js
@@ -1,7 +1,10 @@
 import {uniq} from "lodash";
 import insane from "insane";
 import jsonp from "jsonp";
+import { apiKey, showSmiley}   from './src/config';
+console.log('🤹‍♂️', apiKey);
 
+showSmiley('Heelooooo');
 
 const ages = [1,3,12,45,33,66,89,65,22];
 console.log(uniq(ages));
---
+++ 13-es6-modules/src/config.js
@@ -0,0 +1,12 @@
+// * default export
+/*
+const apiKey = '1234';
+export default apiKey;
+*/
+
+// * named export
+export const apiKey = '123';
+
+export function showSmiley(str) {
+    console.log('😀', str);
+}
